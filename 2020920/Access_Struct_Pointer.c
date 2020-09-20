ca3362b0167ba866113d8d0a4f90dbfac1e7fd80
Jason-time <noconal+jason-time-github@gmail.com>
2020-09-20 13:35:08 +0900

    Access_Struct_Pointer_anotherWay

---
+++ 2020920/Access_Struct_Pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+struct person{
+	int age;
+	float weight;
+};
+
+int main(){
+	struct person *personPtr,person1;
+	personPtr = &person1;
+	
+	printf("Enter age: ");
+	scanf("%d", &personPtr -> age);
+	
+	printf("Enter weight: ");
+	scanf("%f", &personPtr -> weight);
+	// 구조체 접근 방식 -> 
+	printf("\nDisplaying:\n");
+	printf("Age: %d\n", personPtr->	age);
+	printf("weight: %.1f", personPtr -> weight);
+	
+	// 구조체 접근 방식 (*). 
+	printf("\nDisplaying:\n");
+	printf("Age: %d\n", (*personPtr).age);
+	printf("weight: %.1f", (*personPtr).weight);
+	
+	return 0;	
+}
