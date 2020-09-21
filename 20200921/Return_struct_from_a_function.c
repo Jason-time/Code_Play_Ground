db1302cdb80f24d2265ec318d129b25211e84c7d
Jason-time <noconal+jason-time-github@gmail.com>
2020-09-21 21:26:32 +0900

    Store_Structure_and_Display.c

---
+++ 20200921/Return_struct_from_a_function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+struct student{
+	char name[50];
+	int age;
+};
+
+struct student getInformation();
+
+int main(){
+	struct student stu;
+	stu = getInformation();
+
+	printf("\nDisplaying information\n");
+	printf("Name: %s",stu.name);
+	printf("\nRoll: %d",stu.age);
+
+	return 0;
+}
+
+struct student getInformation(){
+	struct student stu_1;
+	printf("Enter name: ");
+	scanf("%[^\n]%*c",stu_1.name);
+
+	printf("Enter age: ");
+	scanf("%d", &stu_1.age);
+
+	return stu_1;
+}
